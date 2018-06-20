08ac1014661b9f0bd979c5d9014064f0d77c300d
waynedw <greyman@live.cn>
2018-06-19 22:54:08 -0400

    fix tab and spaces inconsistency

--- Solve L x = b with BBTS and Spike/a. Block banded triangular solver to Solve Lx=b/block_banded_triangular_v4.c
+++ Solve L x = b with BBTS and Spike/a. Block banded triangular solver to Solve Lx=b/block_banded_triangular_v4.c
@@ -11,30 +11,30 @@ const int WID = 128;
 
 int main(int argc, char *argv[])
 {
-	int ret_code, chunk, nthreads, tid;
-	int i, j, row[1], col[1], len[1];
-	float **A, *b, *x;
+    int ret_code, chunk, nthreads, tid;
+    int i, j, row[1], col[1], len[1];
+    float **A, *b, *x;
     double *MM;
-	FILE *f1, *f2, *f3;
-	MM_typecode matcode;
-	chunk = 16;
+    FILE *f1, *f2, *f3;
+    MM_typecode matcode;
+    chunk = 16;
 
     /* Input File Judgement */
-	if (argc < 4) {
-		fprintf(stderr, "Usage: %s missing input data!\n", argv[0]);
-		exit(1);
-	}
-	else {
-		if ((f1 = fopen(argv[1], "r")) == NULL)
-			exit(1);
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s missing input data!\n", argv[0]);
+        exit(1);
+    }
+    else {
+        if ((f1 = fopen(argv[1], "r")) == NULL)
+            exit(1);
         if ((f2 = fopen(argv[2], "r")) == NULL)
             exit(1);
         if ((f3 = fopen(argv[3], "w")) == NULL)
             exit(1);
-	}
-	/* Read Input File */
+    }
+    /* Read Input File */
     printf("Reading...\n");
-	A = readBandedMatrix(f1, &matcode, row, col, len, WID);
+    A = readBandedMatrix(f1, &matcode, row, col, len, WID);
     b = readVector(f2, &matcode, row);
     printf("Runing...\n");
     // Algorithm 3.3: Block Banded Triangular solver
@@ -43,5 +43,5 @@ int main(int argc, char *argv[])
     double  toc = omp_get_wtime();
     printf("Time = %.3f seconds\n", toc - tic);
     fileWrite(f3, row, x, &matcode);
-	return 0;
+    return 0;
 }
